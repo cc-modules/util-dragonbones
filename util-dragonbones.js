9f1a136cf2795aaec5d15f8e0dff8a209f7bd158
wangweihua <wangweihua@vipkid.com.cn>
2020-03-16 14:58:21 +0800

    big bang

---
+++ util-dragonbones.js
@@ -0,0 +1,30 @@
+function playAnimation(node, anim, playTimes = 1, nextAnim) {
+  const armatureDispaly = node.getComponent(dragonBones.ArmatureDisplay);
+  return new Promise((rs) => {
+    armatureDispaly.playAnimation(anim, playTimes);
+    armatureDispaly.once('complete', (event) => {
+      rs(event);
+      if (nextAnim) armatureDispaly.playAnimation(nextAnim);
+    }, this);
+  });
+}
+
+export default {
+  playAnimation: playAnimation,
+  $inject(nodes, prefix = '') {
+    if (!Array.isArray(nodes) && cc.Node.isNode(nodes)) {
+      nodes = [nodes];
+    }
+    nodes.forEach((node) => {
+      Object.keys(this).forEach(key => {
+        if (key.match(/^\$|[0-9_]$/)) return;
+        const newKey = `${prefix}${key}`;
+        if (typeof node[newKey] !== 'undefined') {
+          console.warn(`${newKye} already defined on node. Consider using $inject(..., prefix)`);
+        } else {
+          node[newKey] = this[key].bind(node, node);
+        }
+      });
+    });
+  }
+};
