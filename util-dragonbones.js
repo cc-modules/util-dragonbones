bcac4422e66f753e66705600bc39243295f90c27
wangweihua <wangweihua@vipkid.com.cn>
2020-03-23 18:31:10 +0800

    feat: add $verbalize()

--- util-dragonbones.js
+++ util-dragonbones.js
@@ -3,14 +3,42 @@ function playAnimation(node, anim, playTimes = 1, nextAnim) {
   return new Promise((rs) => {
     armatureDispaly.playAnimation(anim, playTimes);
     armatureDispaly.once('complete', (event) => {
-      rs(event);
-      if (nextAnim) armatureDispaly.playAnimation(nextAnim);
+      if (nextAnim) {
+        rs(armatureDispaly.playAnimation(nextAnim));
+      } else {
+        rs(event);
+      }
     }, this);
   });
 }
 
+function verbalize(node, anims) {
+  const nodes = Array.isArray(node) ? node : [node];
+  if (Array.isArray(anims)) {
+    anims.forEach(anim => {
+      nodes.forEach(node => {
+        node[anim] = function (...args) {
+          return playAnimation(node, anim, ...args);
+        };
+      });
+    });
+  } else {
+    for (let alias in anims) {
+      nodes.forEach(node => {
+        const method = alias;
+        const anim = anims[method];
+        node[method] = function (...args) {
+          return playAnimation(node, anim, ...args);
+        };
+      });
+    }
+  }
+  return node;
+}
+
 export default {
   playAnimation: playAnimation,
+  $verbolize: verbalize,
   $inject(nodes, prefix = '') {
     if (!Array.isArray(nodes) && cc.Node.isNode(nodes)) {
       nodes = [nodes];
